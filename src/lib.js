70a39aeacf6eae8a247859d862bc2215d42b1346
Jam Risser <jam@jamrizzi.com>
2018-02-20 19:37:15 +0000

    Use babel instead of nwb

--- src/lib.js
+++ src/lib.js
@@ -1,52 +1,52 @@
-import emptyDir from "empty-dir";
-import gitUserEmail from "git-user-email";
-import gitUserName from "git-user-name";
+import emptyDir from 'empty-dir';
+import gitUserEmail from 'git-user-email';
+import gitUserName from 'git-user-name';
 
 export function isEmpty() {
   return emptyDir.sync(process.cwd());
 }
 
 export function guessEmail() {
-  return gitUserEmail() || "email@example.com";
+  return gitUserEmail() || 'email@example.com';
 }
 
 export function guessUsername(email) {
   const matches = (email || guessEmail()).match(/^[^@]+/g);
   if (matches.length > 0) return matches[0];
-  return "some-username";
+  return 'some-username';
 }
 
 export function guessName() {
   const matches = process.cwd().match(/[^\/]+$/g);
   if (isEmpty() && matches.length > 0) return matches[0];
-  return "some-name";
+  return 'some-name';
 }
 
 export function guessAuthorName() {
-  return gitUserName() || "Some Name";
+  return gitUserName() || 'Some Name';
 }
 
 export function copy(yo) {
   return Promise.all([
     yo.fs.copyTpl(
-      yo.templatePath("template/shared/README.md"),
-      yo.destinationPath("README.md"),
-      ...yo.context
+      yo.templatePath('template/shared/README.md'),
+      yo.destinationPath('README.md'),
+      { ...yo.context }
     ),
     yo.fs.copyTpl(
-      yo.templatePath("template/shared/CHANGELOG.md"),
-      yo.destinationPath("CHANGELOG.md"),
-      ...yo.context
+      yo.templatePath('template/shared/CHANGELOG.md'),
+      yo.destinationPath('CHANGELOG.md'),
+      { ...yo.context }
     ),
     yo.fs.copyTpl(
-      yo.templatePath("template/shared/CONTRIBUTING.md"),
-      yo.destinationPath("CONTRIBUTING.md"),
-      ...yo.context
+      yo.templatePath('template/shared/CONTRIBUTING.md'),
+      yo.destinationPath('CONTRIBUTING.md'),
+      { ...yo.context }
     ),
     yo.fs.copyTpl(
-      yo.templatePath("template/shared/LICENSE"),
-      yo.destinationPath("LICENSE"),
-      ...yo.context
+      yo.templatePath('template/shared/LICENSE'),
+      yo.destinationPath('LICENSE'),
+      { ...yo.context }
     )
   ]);
 }
--- tests/index-test.js
+++
@@ -1,8 +0,0 @@
-import expect from 'expect';
-import message from '../src';
-
-describe('Module template', () => {
-  it('displays a welcome message', () => {
-    expect(message).toContain('Welcome to generator-yohoho');
-  });
-});
